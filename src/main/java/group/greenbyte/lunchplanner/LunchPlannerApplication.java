4a222b2a0b63979105efe228d584b5c401fcf394
SchalterDev <schalter.dev@gmail.com>
2018-04-03 14:03:45 +0200

    Initial commit

---
+++ src/main/java/group/greenbyte/lunchplanner/LunchPlannerApplication.java
@@ -0,0 +1,12 @@
+package group.greenbyte.lunchplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchPlannerApplication.class, args);
+    }
+}
