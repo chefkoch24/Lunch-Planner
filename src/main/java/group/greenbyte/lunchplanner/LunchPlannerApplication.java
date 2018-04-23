e1575323a5b11ad978142393d32fba1e2d5ede57
SchalterDev <schalter.dev@gmail.com>
2018-04-23 11:32:20 +0200

    Funktionierende config

--- src/main/java/group/greenbyte/lunchplanner/LunchPlannerApplication.java
+++ src/main/java/group/greenbyte/lunchplanner/LunchPlannerApplication.java
@@ -2,9 +2,10 @@ package group.greenbyte.lunchplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LunchPlannerApplication {
+public class LunchPlannerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(LunchPlannerApplication.class, args);
---
+++ src/main/java/group/greenbyte/lunchplanner/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package group.greenbyte.lunchplanner.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/registration").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
+    }
+
+}
